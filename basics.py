2032985be0c4c0c09c1557701656429cbe839e9d
Sashankr <sashankr97@gmail.com>
2025-12-16 23:53:33 +0530

    syntax

--- basics.py
+++ basics.py
@@ -1,2 +1,4 @@
 print("Greetings, adventurer")
 print(10+20)
+# Syntax is rules for valid code.
+# When we run code with invalid syntax, the Python interpreter throws an error message and execution stops.
