47aa9c96f22dfb69326840afb984e3e99495dd37
Sashankr <sashankr97@gmail.com>
2025-12-17 00:04:54 +0530

    math operator

--- basics.py
+++ basics.py
@@ -3,4 +3,9 @@ print(10+20)
 # Syntax is rules for valid code.
 # When we run code with invalid syntax, the Python interpreter throws an error message and execution stops.
 player_health = 1000
-print(player_health);
+print(player_health)
+
+# Python has basic math, add, subtract, divide and multiply.
+armor_multiplier = 2
+armored_health = player_health * armor_multiplier
+print('Armored Health',armored_health)
