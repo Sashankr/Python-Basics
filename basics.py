4c8fb13465df31c1578fdd3ceaf7d816504410ef
Sashankr <sashankr97@gmail.com>
2025-12-16 23:59:26 +0530

    declaring and using variables

--- basics.py
+++ basics.py
@@ -2,3 +2,5 @@ print("Greetings, adventurer")
 print(10+20)
 # Syntax is rules for valid code.
 # When we run code with invalid syntax, the Python interpreter throws an error message and execution stops.
+player_health = 1000
+print(player_health);
