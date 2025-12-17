cef62457dfeaa95ca7e61f784dc143df210fe8c9
Sashankr <sashankr97@gmail.com>
2025-12-18 00:03:48 +0530

    None is returned if we don't return anything from a function

--- functions.py
+++ functions.py
@@ -10,3 +10,11 @@ output = area_of_circle(10);
 print(f"Area of circle is {output}")
 
 # We cannot call functions without it being defined first.
+
+# When we don't return anything from a function None is returned.
+
+def sum():
+    print('Sum called')
+
+sum_res = sum();
+print(sum_res)
